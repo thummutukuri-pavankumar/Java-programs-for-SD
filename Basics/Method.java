82b3a90d7c73f1450a94fcad1ce66485a26929d8
thummutukuri-pavankumar <kumarpavan91694@gmail.com>
2025-05-23 15:32:24 +0530

    this program explains about how to create and call an instance of a method.

---
+++ Basics/Method.java
@@ -0,0 +1,16 @@
+public class Geeks
+{
+    // Method to print message
+    public void printMessage() {
+        System.out.println("Hello, Geeks!");
+    }
+
+    public static void main(String[] args) {
+      
+        // Create an instance of the Method class
+        Geeks obj = new Geeks();
+        
+        // Calling the method
+        obj.printMessage(); 
+    }
+}
